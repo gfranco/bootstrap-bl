556992317f45d1fa077a9f65fd4af7b43804c22a
Georgiy Mostolovitsa <hello@gfranco.ru>
2012-08-26 17:30:46 +0300

    Base structure

---
+++ pages-desktop/all/all.deps.js
@@ -0,0 +1,104 @@
+exports.deps = [
+    {
+        "block": "i-global"
+    },
+    {
+        "block": "reset"
+    },
+    {
+        "block": "variables"
+    },
+    {
+        "block": "mixins"
+    },
+    {
+        "block": "scaffolding"
+    },
+    {
+        "block": "grid"
+    },
+    {
+        "block": "layout"
+    },
+    {
+        "block": "type"
+    },
+    {
+        "block": "code"
+    },
+    {
+        "block": "form"
+    },
+    {
+        "block": "table"
+    },
+    {
+        "block": "sprites"
+    },
+    {
+        "block": "dropdowns"
+    },
+    {
+        "block": "wells"
+    },
+    {
+        "block": "component-animations"
+    },
+    {
+        "block": "close"
+    },
+    {
+        "block": "buttons"
+    },
+    {
+        "block": "button-groups"
+    },
+    {
+        "block": "alerts"
+    },
+    {
+        "block": "navs"
+    },
+    {
+        "block": "navbar"
+    },
+    {
+        "block": "breadcrumbs"
+    },
+    {
+        "block": "pagination"
+    },
+    {
+        "block": "pager"
+    },
+    {
+        "block": "modals"
+    },
+    {
+        "block": "tooltip"
+    },
+    {
+        "block": "popovers"
+    },
+    {
+        "block": "thumbnails"
+    },
+    {
+        "block": "labels-badges"
+    },
+    {
+        "block": "progress-bars"
+    },
+    {
+        "block": "accordion"
+    },
+    {
+        "block": "carousel"
+    },
+    {
+        "block": "hero-unit"
+    }
+];
+exports.depsByTechs = {
+    "": {}
+};
