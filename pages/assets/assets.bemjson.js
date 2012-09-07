800713c58d48a77eef76a785dc5f9816dda0ad9b
Georgiy Mostolovitsa <hello@gfranco.ru>
2012-09-08 03:33:45 +0400

    site refactor

---
+++ pages/assets/assets.bemjson.js
@@ -0,0 +1,50 @@
+({
+    block: 'b-page',
+    title: 'Say hello to Bootstrap',
+    content: [
+        {
+            block: 'b-head'
+        },
+        {
+            block: 'b-grid'
+        },
+        {
+            block: 'b-section'
+        },
+        {
+            block: 'table',
+            mods: { bordered: 'yes', striped: 'yes' }
+        },
+        {
+            block: 'label',
+            mods: { badge: 'yes', type: 'success' }
+        },
+        {
+            block: 'label',
+            mods: { badge: 'yes', type: 'warning' }
+        },
+        {
+            block: 'label',
+            mods: { badge: 'yes', type: 'important' }
+        },
+        {
+            block: 'btn',
+            mods: { type: 'primary', size: 'large' }
+        },
+        {
+            block: 'btn',
+            mods: { type: 'success', size: 'small' }
+        },
+        {
+            block: 'btn',
+            mods: { type: 'warning' }
+        },
+        {
+            block: 'b-example'
+        },
+        {
+            block: 'code',
+            mods: { type: 'block' }
+        }
+    ]
+})
